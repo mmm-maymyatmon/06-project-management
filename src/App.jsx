84ac20ce6271bcdb4dc55f17f19e23a3331e2353
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-01-25 17:52:41 +0700

    ✅ Add project details and handling project delete
    add project details and handling project delete

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectsState, setProjectState] = useState({
     selectedProjectId: undefined,
     projects: [],
   });
+
+  function handleSelectProject(id) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  }
 
   function handleStartAddProject() {
     setProjectState((prevState) => {
@@ -26,6 +36,7 @@ function App() {
       };
     });
   }
+  
 
   function handleAddProject(projectData) {
     setProjectState((prevState) => {
@@ -42,18 +53,40 @@ function App() {
     });
   }
 
-  let content;
+  function handleDeleteProject(id) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project)=> project.id !== prevState.selectedProjectId
+        )
+      };
+    })
+  }
+
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelProject} />;
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelProject} />
+    );
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8 ">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
+      />
       <div className="w-2/3 p-10 bg-gradient-to-br from-stone-50 to-stone-200 shadow-xl rounded-lg">
-      {content}
+        {content}
       </div>
     </main>
   );
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,33 +1,43 @@
 import React from "react";
 import Button from "./Button";
 
-const ProjectsSidebar = ({ onStartAddProject, projects }) => {
+const ProjectsSidebar = ({
+  onStartAddProject,
+  projects,
+  onSelectProject,
+  selectedProjectId,
+}) => {
   return (
     <aside className="w-1/3 bg-gradient-to-br from-stone-200 to-stone-300 px-8 py-10 rounded-r-3xl text-stone-800 shadow-2xl md:w-72">
       <h2 className="mb-8 text-2xl font-bold uppercase tracking-wide text-stone-700 md:text-2xl">
         Your Projects
       </h2>
       <div className="space-y-8">
-        <Button
-          onClick={onStartAddProject}
-        >
-          + Add Project
-        </Button>
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
         <ul className="space-y-4">
-          {projects.map((project) => (
-            <li
-              key={project.id}
-              className="flex items-center justify-between bg-stone-100 px-5 py-3 rounded-lg shadow-md hover:shadow-lg hover:bg-amber-50 transition duration-300 ease-in-out"
-            >
-              <span className="text-sm font-medium text-stone-800 truncate">{project.title}</span>
-              <button
-                className="text-stone-500 hover:text-amber-700 focus:outline-none transition"
-                title="Edit Project"
-              >
-                ✏️
-              </button>
-            </li>
-          ))}
+          {projects.map((project) => {
+            let cssClasses =
+              "flex items-center justify-between px-4 py-3 rounded-lg shadow-md transition duration-300 ease-in-out";
+            if (project.id === selectedProjectId) {
+              cssClasses += "bg-stone-800 text-stone-200";
+            } else {
+              cssClasses +=
+                "bg-stone-100 text-stone-600 hover:bg-amber-50 hover:text-amber-700";
+            }
+            return (
+              <li key={project.id} className={cssClasses}>
+                <span className="text-sm font-medium text-stone-800 truncate">
+                  {project.title}
+                </span>
+                <button
+                  onClick={()=> onSelectProject(project.id)}
+                  title="Edit Project"
+                >
+                  ✏️
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </aside>
